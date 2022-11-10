b3cc0145c4af8501d0b88da188c444a51be4557f
UrazbaevT <utilek990@gmail.com>
2022-11-10 18:05:09 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        createObject(Class.SAMSUNG, "Samsung", "Li Ben Chol", 1938, 8900, "South Korea, Seoul", 0.0, 0, 0, null);
-        createObject(Class.APPLE, "Apple", "Stive Jobs", 1976, 0, null, 366.7, 389, 0, null);
-        createObject(Class.XIAOMI, "Xiaomi", "Lei Jun", 2010, 0, null, 0.0, 0, 16683, "Redmi");
+        createObject("Samsung");
+        createObject("Apple");
+        createObject("Xiaomi");
 
     }
 
-    private static Phone createObject(Class typeOfClass, String name, String founder, int year, int workersWages, String companyLocation, double turnoverPerYear, int numberOfCompanies, int numberOfWorkers, String performanceOtherPhones) {
-        if (Class.SAMSUNG == typeOfClass) {
-            Samsung samsung = new Samsung("Samsung", "Li Ben Chol", 1938, 8900, "South Korea, Seoul");
-            samsung.print();
-            return samsung;
-        }else if (Class.APPLE == typeOfClass){
-            Apple apple = new Apple("Apple", "Stive Jobs", 1976, 366.7, 389);
-            apple.print();
-            return apple;
-        }else if (Class.XIAOMI == typeOfClass){
-            Xiaomi xiaomi = new Xiaomi("Xiaomi", "Lei Jun", 2010, 16683, "Redmi");
-            xiaomi.print();
-            return xiaomi;
-        }else {
-            return null;
+    private static Phone createObject(String ClassName) {
+        switch (ClassName) {
+            case "Samsung":
+                Samsung samsung = new Samsung("Samsung", "Li Ben Chol", 1938, 8900, "South Korea, Seoul");
+                samsung.print();
+                break;
+            case "Apple":
+                Apple apple = new Apple("Apple", "Stive Jobs", 1976, 366.7, 389);
+                apple.print();
+                break;
+            case "Xiaomi":
+                Xiaomi xiaomi = new Xiaomi("Xiaomi", "Lei Jun", 2010, 16683, "Redmi");
+                xiaomi.print();
+                break;
         }
+        return null;
     }
 }
